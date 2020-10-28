2c29d40a8138f224d9cc3d5a021efcf87b8def73
Alexandre Gros <xelaflash@gmail.com>
2020-10-28 12:06:35 -0400

    🔨 fix active tab

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,11 +1,12 @@
 import 'jquery';
 import 'bootstrap';
 
-const current = document.location.pathname.split('/')[2];
+const current = document.location.pathname.split('/')[1];
+
 const tabs = document.querySelectorAll('.tab');
 
 Array.from(tabs).forEach(function(el) {
-  const truncatedHref = el.href.split('/')[4];
+  const truncatedHref = el.href.split('/')[3];
   if (truncatedHref === current) {
     el.classList.add('active');
   }
