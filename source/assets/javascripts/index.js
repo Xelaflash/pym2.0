3e84e1580c0d0c1362c9aeef4ca73beee4dbf161
Alexandre Gros <xelaflash@gmail.com>
2020-10-28 11:32:42 -0400

    🚀 add maps

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,2 +1,26 @@
 import 'jquery';
 import 'bootstrap';
+
+const current = document.location.pathname.split('/')[2];
+const tabs = document.querySelectorAll('.tab');
+
+Array.from(tabs).forEach(function(el) {
+  const truncatedHref = el.href.split('/')[4];
+  if (truncatedHref === current) {
+    el.classList.add('active');
+  }
+});
+
+function initMap() {
+  const cabinet = { lat: 43.477026, lng: -1.506039 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: cabinet,
+  });
+  const marker = new google.maps.Marker({
+  position: cabinet,
+  map,
+  });
+}
+
+global.initMap = initMap;
